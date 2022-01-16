297812e53423aeea01e389210d4ff05d122d84fc
poocong <81405011+poocong@users.noreply.github.com>
2022-01-16 21:54:10 +0700

    update anti_spambot.py

--- userbot/modules/anti_spambot.py
+++ userbot/modules/anti_spambot.py
@@ -11,7 +11,14 @@ from requests import get
 from telethon.events import ChatAction
 from telethon.tl.types import ChannelParticipantsAdmins, Message
 
-from userbot import ANTI_SPAMBOT, ANTI_SPAMBOT_SHOUT, BOTLOG_CHATID, CMD_HELP, bot
+from userbot import (
+    ANTI_SPAMBOT,
+    ANTI_SPAMBOT_SHOUT,
+    BOTLOG,
+    BOTLOG_CHATID,
+    CMD_HELP,
+    bot,
+)
 
 
 @bot.on(ChatAction)
@@ -166,7 +173,7 @@ async def ANTI_SPAMBOTS(welcm):
                     )
                     kicked = False
                     reported = True
-                if BOTLOG_CHATID and (kicked or reported):
+                if BOTLOG and (kicked or reported):
                     await welcm.client.send_message(
                         BOTLOG_CHATID,
                         "#ANTI_SPAMBOT REPORT\n"
