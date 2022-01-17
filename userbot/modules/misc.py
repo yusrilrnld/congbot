4a7359847c90ec3874ab3fffc761a5a9eaa3b7aa
rzkyfadilla._ <81405011+poocong@users.noreply.github.com>
2022-01-17 15:34:29 +0700

    just beautify

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -126,10 +126,10 @@ async def string_is_here(event):
     await edit_or_reply(
         event,
         "**List Bot**\n"
-        "\n•[❏ **GET STRING SESSION TELEGRAM :**"
-        "\n[[ KLIK DISINI ]](https://t.me/StringPocongBot)"
-        "\n•[❏ **GET API ID & API HASH :**"
-        "\n[[ KLIK DISINI ]](https://t.me/pocongscrapbot)",
+        "\n┌❏**GET STRING BOT:**"
+        "\n├❏ [[ KLIK DISINI ]](https://t.me/StringPocongBot)"
+        "\n├❏ **GET API ID & API HASH :**"
+        "\n└❏ [[ KLIK DISINI ]](https://t.me/pocongscrapbot)",
     )
 
 
