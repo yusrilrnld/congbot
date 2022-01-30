ee47a8cd659699970f742affc54417f5ef92af4e
rzkyfadilla._ <81405011+poocong@users.noreply.github.com>
2022-01-30 14:55:17 +0700

    PocongUserbot: fix if userbot is not deployed on heroku

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -21,7 +21,7 @@ from PIL import Image
 from userbot import BOT_VER, BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, SUDO_USERS, branch
-from userbot.utils import edit_or_reply, time_formatter
+from userbot.utils import edit_or_reply, poci_cmd, time_formatter
 
 # ================= CONSTANT =================
 if HEROKU_APP_NAME is not None and HEROKU_API_KEY is not None:
