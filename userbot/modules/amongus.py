871d3f6ec2d374c9b146f43298b5e19dddd0cd12
poocong <akuuntumball01@gmail.com>
2022-02-12 05:32:12 +0000

    PocongUserbot: auto fixes
    Signed-off-by: poocong <akuuntumball01@gmail.com>

--- userbot/modules/amongus.py
+++ userbot/modules/amongus.py
@@ -11,7 +11,7 @@ from PIL import Image, ImageDraw, ImageFont
 from requests import get
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, bot
+from userbot import CMD_HELP
 from userbot.utils import poci_cmd
 
 
