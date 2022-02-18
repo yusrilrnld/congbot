debccec9559701c93d8fd242dbba8ea65618a0e0
poocong <akuuntumball01@gmail.com>
2022-02-18 07:55:29 +0000

    PocongUserbot: auto fixes
    Signed-off-by: poocong <akuuntumball01@gmail.com>

--- userbot/events.py
+++ userbot/events.py
@@ -16,7 +16,7 @@ from traceback import format_exc
 
 from telethon import events
 
-from userbot import CMD_HANDLER, CMD_LIST, DEFAULT, DEVS, bot
+from userbot import CMD_HANDLER, CMD_LIST, DEFAULT, bot
 
 
 def poci_cmd(pattern=None, command=None, **args):
