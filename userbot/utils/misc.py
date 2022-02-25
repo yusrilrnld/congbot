f4a345adbdd8ad9d7332367b9f696f558dab6246
pocong <81405011+poocong@users.noreply.github.com>
2022-02-25 17:11:00 +0700

    .

--- userbot/utils/misc.py
+++ userbot/utils/misc.py
@@ -207,11 +207,11 @@ async def animator(media, mainevent, textevent):
     w, h = (-1, 512) if h > w else (512, -1)
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
-    await mainevent.client.download_media(media, TEMP_DOWNLOAD_DIRECTORY)
+    poci = await mainevent.client.download_media(media, TEMP_DOWNLOAD_DIRECTORY)
     await textevent.edit("`Converting...`")
     await runcmd(
-        f"ffmpeg -ss 00:00:00 -to 00:00:02.900 -i {Risman} -vf scale={w}:{h} -c:v libvpx-vp9 -crf 30 -b:v 560k -maxrate 560k -bufsize 256k -an Video.webm"
+        f"ffmpeg -ss 00:00:00 -to 00:00:02.900 -i {poci} -vf scale={w}:{h} -c:v libvpx-vp9 -crf 30 -b:v 560k -maxrate 560k -bufsize 256k -an Video.webm"
     )
-    os.remove(Risman)
+    os.remove(poci)
     vid = "Video.webm"
     return vid
