08c951a5ac83b400dcd8378efd30a16653f9dcf0
poocong <akuuntumball01@gmail.com>
2022-02-25 09:34:05 +0000

    PocongUserbot: auto fixes
    Signed-off-by: poocong <akuuntumball01@gmail.com>

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -11,7 +11,7 @@ from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
 from userbot import CMD_HANDLER as cmd
 from userbot.events import register
-from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, UPSTREAM_REPO_URL, bot
+from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME, UPSTREAM_REPO_URL
 from userbot.events import poci_cmd
 
 
