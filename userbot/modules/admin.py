a20f3c6dba2f7266aef7e3c5cc56ff2927aa9b87
pocong <81405011+poocong@users.noreply.github.com>
2022-03-22 22:16:50 +0700

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 
-
+import logging
 from asyncio import sleep
 
 from telethon.errors import (
