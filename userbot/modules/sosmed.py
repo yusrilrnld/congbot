f8b275e1d4e2620c685df28df398d8cab1191263
pocong <81405011+poocong@users.noreply.github.com>
2022-04-10 17:39:13 +0700

    Rename tiktok.py to sosmed.py

--- userbot/modules/sosmed.py
+++ userbot/modules/sosmed.py
@@ -1,98 +1,159 @@
-# Port by Koala 🐨/@manuskarakitann
-# Copyright © @mrismanaziz
-# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot/>
-# Recode by @Pocongonlen
-# Nyenyenye bacot
+# Copyright (C) 2020 Frizzy.
+# All rights reserved.
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
 
-from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest
-from telethon.tl.functions.messages import DeleteHistoryRequest
-
+from telethon import events
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.utils import edit_delete, edit_or_reply, poci_cmd
 
 
-@poci_cmd(pattern="sosmed(?: |$)(.*)")
+@poci_cmd(pattern="^.ig(?: |$)(.*)")
 async def insta(event):
-    xxnx = event.pattern_match.group(1)
-    if xxnx:
-        link = xxnx
-    elif event.is_reply:
-        link = await event.get_reply_message()
-    else:
-        return await edit_delete(
-            event,
-            "**Berikan Link Sosmed atau Reply Link Sosmed Untuk di Download**",
-        )
-    xx = await edit_or_reply(event, "`Processing Download...`")
-    chat = "@SaveAsbot"
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await event.edit("`Mohon Reply Ke Link Instagram`")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.text:
+        await event.edit("`Mohon Maaf, Saya Membutuhkan Link Media Instagram Untuk Download`")
+        return
+    chat = "@SaveAsBot"
+    reply_message.sender
+    if reply_message.sender.bot:
+        await event.edit("`Memproses....`")
+        return
+    await event.edit("`Memproses.....`")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=523131145)
             )
-            await event.client.send_message(chat, link)
+            await event.client.send_message(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.client(UnblockRequest(chat))
-            await event.client.send_message(chat, link)
-            response = await response
+            await event.edit("`Mohon Buka Blokir` @SaveAsbot `Lalu Coba Lagi`")
+            return
         if response.text.startswith("Forward"):
-            await xx.edit("Forward Private .")
+            await event.edit(
+                "Uhmm Sepertinya Private."
+            )
         else:
-            await xx.delete()
+            await event.delete()
             await event.client.send_file(
                 event.chat_id,
                 response.message.media,
+                caption=f"**Creator @Pocongonlen**",
             )
             await event.client.send_read_acknowledge(conv.chat_id)
-            await event.client(DeleteHistoryRequest(peer=chat, max_id=0))
-            await xx.delete()
+            await bot(functions.messages.DeleteHistoryRequest(peer=chat, max_id=0))
+            await event.delete()
 
 
-@poci_cmd(pattern="dez(?: |$)(.*)")
-async def DeezLoader(event):
-    if event.fwd_from:
-        return
-    dlink = event.pattern_match.group(1)
-    if ".com" not in dlink:
-        await edit_delete(
-            event, "`Mohon Berikan Link Deezloader yang ingin di download`"
-        )
+@poci_cmd(pattern="tiktok(?: |$)(.*)")
+async def _(event):
+    xxnx = event.pattern_match.group(1)
+    if xxnx:
+        d_link = xxnx
+    elif event.is_reply:
+        d_link = await event.get_reply_message()
     else:
-        await edit_or_reply(event, "`Sedang Mendownload Lagu...`")
-    chat = "@DeezLoadBot"
+        return await edit_delete(
+            event,
+            "**Berikan Link Tiktok Pesan atau Reply Link Tiktok Untuk di Download**",
+        )
+    xx = await edit_or_reply(event, "`Video Sedang Diproses...`")
+    chat = "@thisvidbot"
     async with event.client.conversation(chat) as conv:
         try:
-            await conv.send_message("/start")
-            await conv.get_response()
-            await conv.get_response()
-            await conv.send_message(dlink)
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
             details = await conv.get_response()
-            song = await conv.get_response()
+            video = await conv.get_response()
+            text = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.client(UnblockRequest(chat))
-            await conv.send_message("/start")
-            await conv.get_response()
-            await conv.get_response()
-            await conv.send_message(dlink)
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
             details = await conv.get_response()
-            song = await conv.get_response()
+            video = await conv.get_response()
+            text = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
-        await event.client.send_file(event.chat_id, song, caption=details.text)
-        await event.delete()
+        await event.client.send_file(event.chat_id, video)
+        await event.client.delete_messages(
+            conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id, text.id]
+        )
+        await xx.delete()
 
+
+@poci_cmd(pattern="fb(?: |$)(.*)")
+async def _(event):
+    xxnx = event.pattern_match.group(1)
+    if xxnx:
+        d_link = xxnx
+    elif event.is_reply:
+        d_link = await event.get_reply_message()
+    else:
+        return await edit_delete(
+            event,
+            "**Berikan Link Facebook atau Reply Link Facebook Untuk di Download**",
+        )
+    xx = await edit_or_reply(event, "`Video Sedang Diproses...`")
+    chat = "@thisvidbot"
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            video = await conv.get_response()
+            text = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.client(UnblockRequest(chat))
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            video = await conv.get_response()
+            text = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        await event.client.send_file(event.chat_id, video)
+        await event.client.delete_messages(
+            conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id, text.id]
+        )
+        await xx.delete()
 
 CMD_HELP.update(
     {
-        "sosmed": f"**Plugin : **`sosmed`\
-        \n\n  •  **Syntax :** `{cmd}sosmed` <link>\
-        \n  •  **Function : **Download Media Dari Pinterest / Tiktok / Instagram.\
-        \n\n  •  **Syntax :** `{cmd}dez` <link>\
-        \n  •  **Function : **Download Lagu Via Deezloader\
+        "tiktok": f"**Plugin : **`tiktok`\
+        \n\n  •  **Syntax :** `{cmd}tiktok` <link>\
+        \n  •  **Function : **Download Video Tiktok Tanpa Watermark\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "fb": f"**Plugin : **`Facebook`\
+        \n\n  •  **Syntax :** `{cmd}fb` <link>\
+        \n  •  **Function : **Download Video dari Facebook\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "instagram": f"**Plugin : **`instagram`\
+        \n\n  •  **Syntax :** `{cmd}ig` <reply di link>\
+        \n  •  **Function: **Download Video Instagram\
     "
     }
 )
