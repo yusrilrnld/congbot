42adb014a4e2f78b20a54ab704bbd0bc9cd16404
pocong <81405011+poocong@users.noreply.github.com>
2022-04-10 17:46:59 +0700

    p

--- userbot/modules/sosmed.py
+++ userbot/modules/sosmed.py
@@ -1,10 +1,13 @@
-# Copyright (C) 2020 Frizzy.
+# Copyright (C) Pocongonlen
+# PocongUserbot < https://github.com/poocong/PocongUserbot >
+
 # All rights reserved.
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest
+from telethon.tl.functions.messages import DeleteHistoryRequest
 from telethon import events
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
@@ -131,6 +134,40 @@ async def _(event):
         )
         await xx.delete()
 
+
+@poci_cmd(pattern="dez(?: |$)(.*)")
+async def DeezLoader(event):
+    if event.fwd_from:
+        return
+    dlink = event.pattern_match.group(1)
+    if ".com" not in dlink:
+        await edit_delete(
+            event, "`Mohon Berikan Link Deezloader yang ingin di download`"
+        )
+    else:
+        await edit_or_reply(event, "`Sedang Mendownload Lagu...`")
+    chat = "@DeezLoadBot"
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message("/start")
+            await conv.get_response()
+            await conv.get_response()
+            await conv.send_message(dlink)
+            details = await conv.get_response()
+            song = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.client(UnblockRequest(chat))
+            await conv.send_message("/start")
+            await conv.get_response()
+            await conv.get_response()
+            await conv.send_message(dlink)
+            details = await conv.get_response()
+            song = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        await event.client.send_file(event.chat_id, song, caption=details.text)
+        await event.delete()
+
 CMD_HELP.update(
     {
         "tiktok": f"**Plugin : **`tiktok`\
@@ -142,18 +179,13 @@ CMD_HELP.update(
 
 CMD_HELP.update(
     {
-        "fb": f"**Plugin : **`Facebook`\
+        "sosmed": f"**Plugin : **`sosmed`\
+        \n\n  •  **Syntax :** `{cmd}ig` <reply di link>\
+        \n  •  **Function : **Download Video / Foto Dari Instagram\
         \n\n  •  **Syntax :** `{cmd}fb` <link>\
         \n  •  **Function : **Download Video dari Facebook\
-    "
-    }
-)
-
-CMD_HELP.update(
-    {
-        "instagram": f"**Plugin : **`instagram`\
-        \n\n  •  **Syntax :** `{cmd}ig` <reply di link>\
-        \n  •  **Function: **Download Video Instagram\
+        \n\n  •  **Syntax :** `{cmd}dez` <link>\
+        \n  •  **Function : **Download Lagu Via Deezloader\
     "
     }
 )
