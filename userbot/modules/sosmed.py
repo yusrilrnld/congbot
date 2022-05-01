8e964ed1b9b84e1facfed438eecbb64f93a6e293
pocong <81405011+poocong@users.noreply.github.com>
2022-05-01 15:17:35 +0700

    ga tau jg sy

--- userbot/modules/sosmed.py
+++ userbot/modules/sosmed.py
@@ -13,47 +13,43 @@ from userbot import CMD_HELP
 from userbot.utils import edit_delete, edit_or_reply, poci_cmd
 
 
-@poci_cmd(pattern="^.ig(?: |$)(.*)")
-async def insta(event):
-    if event.fwd_from:
-        return
-    if not event.reply_to_msg_id:
-        await event.edit("`Mohon Berikan Link / Reply Ke Link Instagram`")
-        return
-    reply_message = await event.get_reply_message()
-    if not reply_message.text:
-        await event.edit("`Mohon Maaf, Saya Membutuhkan Link Media Instagram Untuk Download`")
-        return
-    chat = "@ThisVidBot"
-    reply_message.sender
-    if reply_message.sender.bot:
-        await event.edit("`Memproses....`")
-        return
-    await event.edit("`Memproses.....`")
+@poci_cmd(pattern="ig(?: |$)(.*)")
+async def _(event):
+    xxnx = event.pattern_match.group(1)
+    if xxnx:
+        d_link = xxnx
+    elif event.is_reply:
+        d_link = await event.get_reply_message()
+    else:
+        return await edit_delete(
+            event,
+            "**Berikan Link Instagram atau Reply Link Instagram Untuk di Download**",
+        )
+    xx = await edit_or_reply(event, "`Video Sedang Diproses...`")
+    chat = "@thisvidbot"
     async with event.client.conversation(chat) as conv:
         try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1878760241)
-            )
-            await event.client.send_message(chat, reply_message)
-            response = await response
-        except YouBlockedUserError:
-            await event.edit("`Mohon Buka Blokir` @ThisVidBot `Lalu Coba Lagi`")
-            return
-        if response.text.startswith("Forward"):
-            await event.edit(
-                "Uhmm Sepertinya Private."
-            )
-        else:
-            await event.delete()
-            await event.client.send_file(
-                event.chat_id,
-                response.message.media,
-                caption=f"**Maintened Bot By @pocongproject**",
-            )
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            video = await conv.get_response()
+            text = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
-            await bot(functions.messages.DeleteHistoryRequest(peer=chat, max_id=0))
-            await event.delete()
+        except YouBlockedUserError:
+            await event.client(UnblockRequest(chat))
+            msg_start = await conv.send_message("/start")
+            r = await conv.get_response()
+            msg = await conv.send_message(d_link)
+            details = await conv.get_response()
+            video = await conv.get_response()
+            text = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        await event.client.send_file(event.chat_id, video)
+        await event.client.delete_messages(
+            conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id, text.id]
+        )
+        await xx.delete()
 
 
 @poci_cmd(pattern="tiktok(?: |$)(.*)")
