2a8a62fb4ea2786d480830d6ff841e19b1832981
yusril <99790793+yusrilrnld@users.noreply.github.com>
2022-05-12 09:03:00 +0700

    apd bismillah

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -31,122 +31,122 @@ async def _(event):
 
 
 @bot.on(poci_cmd(outgoing=True, pattern='^P(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮'𝐚𝐥𝐚𝐢𝐤𝐮𝐦...", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮'𝐚𝐥𝐚𝐢𝐤𝐮𝐦...", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @poci_cmd(pattern="atg(?: |$)(.*)")
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇.... 𝐆𝐎𝐁𝐋𝐎𝐊𝐊𝐊𝐊𝐊!!!!", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇.... 𝐆𝐎𝐁𝐋𝐎𝐊𝐊𝐊𝐊𝐊!!!!", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^L(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "𝐖𝐚'𝐚𝐥𝐚𝐢𝐤𝐮𝐦𝐬𝐚𝐥𝐚𝐦...", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "𝐖𝐚'𝐚𝐥𝐚𝐢𝐤𝐮𝐦𝐬𝐚𝐥𝐚𝐦...", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @poci_cmd(pattern="ast(?: |$)(.*)")
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇......", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "𝐀𝐒𝐓𝐀𝐆𝐇𝐅𝐈𝐑𝐔𝐋𝐋𝐀𝐇......", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^K(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**KONTOL LO ANAK BAJINGAN BANGSAT BEDEBAH LO NGENTOT ANAK HARAM UTUSAN DAJAL PEMYEMBAH KAMBING HAGO LO, LAHIR DARI MEMEK LONTE LO KONTOL BGST ANJING!!!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**KONTOL LO ANAK BAJINGAN BANGSAT BEDEBAH LO NGENTOT ANAK HARAM UTUSAN DAJAL PEMYEMBAH KAMBING HAGO LO, LAHIR DARI MEMEK LONTE LO KONTOL BGST ANJING!!!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^N(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**NGENTOOOOOTTTTTT MANUSIA CACAT OTAK RENDAHAN GA PUNYA PIKIRAN LO BANGSAT BANGET ANJING, MUKE LU KAYA BIJI ONTA, BADAN LO BUDUKAN ANJINGGG PALALO KUTUAN BGST, MANUSIA HINA KAGA PANTES BANGET LAHIR LU TOLOL, SKALINYA LAHIR MALAH NYEMBAH POHON PISANG, ORAMG BINJAI POHON PISANG DI PUKULIN, LAH LO MALAH DI SEMBAH GOBLOK AMAT LO BABI!!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**NGENTOOOOOTTTTTT MANUSIA CACAT OTAK RENDAHAN GA PUNYA PIKIRAN LO BANGSAT BANGET ANJING, MUKE LU KAYA BIJI ONTA, BADAN LO BUDUKAN ANJINGGG PALALO KUTUAN BGST, MANUSIA HINA KAGA PANTES BANGET LAHIR LU TOLOL, SKALINYA LAHIR MALAH NYEMBAH POHON PISANG, ORAMG BINJAI POHON PISANG DI PUKULIN, LAH LO MALAH DI SEMBAH GOBLOK AMAT LO BABI!!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^B(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**BACOT AMAT LO GOBLOK IQ LO LEBIH RENDAH DARI GUA ANJING, KEMAMPUAN LO AJA MASIH JAUH SAMA STANDAR KEMAMPUAN GUA ANJING, KASTA LO JAUH DI BAWAH GUA, GUA INJEK INJEK PALA LO, SKALIGUS PALA BAPAK LO SAMPE 7 TURUNAN KELUARGA LO ANJING, HIDUP MISKIN GA BERGUNA GA ADA MAMFAAN LO HIDUP ANJING!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**BACOT AMAT LO GOBLOK IQ LO LEBIH RENDAH DARI GUA ANJING, KEMAMPUAN LO AJA MASIH JAUH SAMA STANDAR KEMAMPUAN GUA ANJING, KASTA LO JAUH DI BAWAH GUA, GUA INJEK INJEK PALA LO, SKALIGUS PALA BAPAK LO SAMPE 7 TURUNAN KELUARGA LO ANJING, HIDUP MISKIN GA BERGUNA GA ADA MAMFAAN LO HIDUP ANJING!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^M(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**BHAAAKKKKSSSSSSSS, NGENTOT NGAKAK GUA LIAT KOSA KATA LO YG SAMA SKALI GA BIKIN GETER ANJ, PERCUMA AJA LO BIKIN KOSA KATA PANJANG HAMPIR KAYA KOMIK KOMEDI TOLOL ISINYA LAWACK SEMUA, EMG GUA AKUIN HIDUP LO CERIA, TAPI SANGKING CERIA NYA LO GUA ANGGAP GILA ANJING MANUSIA BEDEBAH, GA ADA KEMAMPUAN BUAT BIKIN GETER MENDING CABUT DEKKUH!!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**BHAAAKKKKSSSSSSSS, NGENTOT NGAKAK GUA LIAT KOSA KATA LO YG SAMA SKALI GA BIKIN GETER ANJ, PERCUMA AJA LO BIKIN KOSA KATA PANJANG HAMPIR KAYA KOMIK KOMEDI TOLOL ISINYA LAWACK SEMUA, EMG GUA AKUIN HIDUP LO CERIA, TAPI SANGKING CERIA NYA LO GUA ANGGAP GILA ANJING MANUSIA BEDEBAH, GA ADA KEMAMPUAN BUAT BIKIN GETER MENDING CABUT DEKKUH!!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^Y(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**YAUDAH IYAAAAAA ASU, BANYAK BACOT BANGET LU MUKA BERUNTUSAN, PIPI LO PADA BELOBANG KAYA BATU KARANG, DIKIT LAGI KELUAR CACING TU DARI PIPI LO ANJING, NAJIS AMAT MUKE LO BOROKAN MULU BGST JIDAD LU BENANAH CUIHH DEH NGENTOD!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**YAUDAH IYAAAAAA ASU, BANYAK BACOT BANGET LU MUKA BERUNTUSAN, PIPI LO PADA BELOBANG KAYA BATU KARANG, DIKIT LAGI KELUAR CACING TU DARI PIPI LO ANJING, NAJIS AMAT MUKE LO BOROKAN MULU BGST JIDAD LU BENANAH CUIHH DEH NGENTOD!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^C(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**MUKA LU HINA, MASIH PAKE PP PINTEREST AJA BELAGU LU NGENTODDDDDD KAYAK TOLOLL, GAUSAH SOK KERAS DAN NGENTOD!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**MUKA LU HINA, MASIH PAKE PP PINTEREST AJA BELAGU LU NGENTODDDDDD KAYAK TOLOLL, GAUSAH SOK KERAS DAN NGENTOD!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^S(?: |$)(.*)')
-async def event(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**GAUSAH SOKAP DEH ANJING, UDAH JELEK MUKA RATA KAYA ASPAL MANDALIKA AJA SOKENAL SAMA GUE LO BAJINGAN!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**GAUSAH SOKAP DEH ANJING, UDAH JELEK MUKA RATA KAYA ASPAL MANDALIKA AJA SOKENAL SAMA GUE LO BAJINGAN!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^V(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**MACAM BAGUS AE LU BEGITU KONTOL, GAUSAH BANYAK GAYA TOLOL LO MEMEK!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**MACAM BAGUS AE LU BEGITU KONTOL, GAUSAH BANYAK GAYA TOLOL LO MEMEK!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^J(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**JELEK BANGET LU, NAJIS CUIHHHH ANAK NGENTOT ANAK HARAM ANAK YATIM ANAK TOLOL!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**JELEK BANGET LU, NAJIS CUIHHHH ANAK NGENTOT ANAK HARAM ANAK YATIM ANAK TOLOL!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^A(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**PASANG PP DULU DEK,BIAR ORANG SEGRUP TAU BETAPA HINA NYA MUKA LU, YANG KELIATAN DARI JAUH KAYA BATANG POHON DI LIAT DARI DEKET KAYA POCONG, AJG AJG😆**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**PASANG PP DULU DEK,BIAR ORANG SEGRUP TAU BETAPA HINA NYA MUKA LU, YANG KELIATAN DARI JAUH KAYA BATANG POHON DI LIAT DARI DEKET KAYA POCONG, AJG AJG😆**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^X(?: |$)(.*)')
-async def _(typew):
-    if typew.chat_id in BLACKLIST_CHAT:
+async def _(event):
+    if event.chat_id in BLACKLIST_CHAT:
         return await edit_or_reply(
-            typew, "**GABISA DISINI NGENTOT!!!**"
+            event, "**GABISA DISINI NGENTOT!!!**"
         )
-    await typew.client.send_message(
-        typew.chat_id, "**GC SAMPAH, MEMBER CULIKAN MANA TYPINGAN SEPI BEGINI ISINYA FORWARD CHANNEL SAMA GCAST DOANG CUIHHHHH!!!!!!!!!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+    await event.client.send_message(
+        event.chat_id, "**GC SAMPAH, MEMBER CULIKAN MANA TYPINGAN SEPI BEGINI ISINYA FORWARD CHANNEL SAMA GCAST DOANG CUIHHHHH!!!!!!!!!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^Z(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**GAUSAH SOK KERAS AJAK WAR TOLOL, KALO MUKA JELEK PASTI MALU VNV, SUARA CEMPRENG GA BISA BACOT MULUT TREMOR GAMPANG KETRIGGER, UDAH GITU NGAJAK COD NAJIS BANGET, KAYA ANAK FACEBOOK ANJING, MENDING LU JUAL BELI REPTIL AJA SANA NGENTOD**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**GAUSAH SOK KERAS AJAK WAR TOLOL, KALO MUKA JELEK PASTI MALU VNV, SUARA CEMPRENG GA BISA BACOT MULUT TREMOR GAMPANG KETRIGGER, UDAH GITU NGAJAK COD NAJIS BANGET, KAYA ANAK FACEBOOK ANJING, MENDING LU JUAL BELI REPTIL AJA SANA NGENTOD**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^H(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**OCEHAN LAWAK KAYA GITU HAMPIR MIRIP BADUT YG LAGI HIBUR BOCIL ULTAH BEGOK, GA BIKIN GUA GETER TOLOL, MENDING LU JUALAN KOPI KELILING PAKE SEPEDA BEGO, BIAR ADA KEMAJUAN WALAU YG BELI JAMET JAMET TONGKRONGAN YAHAHAHA, CUIHHHH!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**OCEHAN LAWAK KAYA GITU HAMPIR MIRIP BADUT YG LAGI HIBUR BOCIL ULTAH BEGOK, GA BIKIN GUA GETER TOLOL, MENDING LU JUALAN KOPI KELILING PAKE SEPEDA BEGO, BIAR ADA KEMAJUAN WALAU YG BELI JAMET JAMET TONGKRONGAN YAHAHAHA, CUIHHHH!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^O(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**JANGAN MAEN BOT MULU, ALAY LU NGENTOTT,KESANNYA NORAK, CUIHHHH!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**JANGAN MAEN BOT MULU, ALAY LU NGENTOTT,KESANNYA NORAK, CUIHHHH!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 @bot.on(poci_cmd(outgoing=True, pattern='^G(?: |$)(.*)')
-async def _(typew):
-    await typew.client.send_message(
-        typew.chat_id, "**GAK ADA KEREN KERENNYA LU BEGITU NGENTOD, BAPAK LU SUJUD SUJUD DI DEPAN GUA NGENTOD GARA GARA KEBINGUNGAN GA BISA NGASIH MAKAN LU, MAKANYA DIA MINTA DI SANTUNIN PLUS DI KASIH BANSOS SAMA GUA BANGSAT!!!**", reply_to=typew.reply_to_msg_id)
-    await typew.delete()
+async def _(event):
+    await event.client.send_message(
+        event.chat_id, "**GAK ADA KEREN KERENNYA LU BEGITU NGENTOD, BAPAK LU SUJUD SUJUD DI DEPAN GUA NGENTOD GARA GARA KEBINGUNGAN GA BISA NGASIH MAKAN LU, MAKANYA DIA MINTA DI SANTUNIN PLUS DI KASIH BANSOS SAMA GUA BANGSAT!!!**", reply_to=event.reply_to_msg_id)
+    await event.delete()
 
 
 CMD_HELP.update(
